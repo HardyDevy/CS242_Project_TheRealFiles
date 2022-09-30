3db45dbf41171bb0f00ec0258dc1fb448710e79e
HardyDevy <dghardy2020@gmail.com>
2022-09-30 14:01:00 -0400

    Implements all methods (except hashcode)

--- src/ClackServer.java
+++ src/ClackServer.java
@@ -4,13 +4,30 @@ public class ClackServer {
     //dataToReceiveFromClient
     //dataToSendToClient
 
-    //ClackServer( port )
-    //ClackServer()
-    //start()
-    //receiveData()
-    //sendData()
-    //getPort()
-    //hashCode()
-    //equals()
-    //toString()
+    ClackServer(int port){
+        this.port = port;
+        //dataToReceiveFromClient = null;
+        //dataToSendToClient = null;
+    }
+    ClackServer(){
+        this(7000);
+    }
+    public void start(){}
+    public void receiveData(){}
+    public void sendData(){}
+    public int getPort(){
+        return this.port;
+    }
+
+    // Need To Implement hashCode and add dataToReceiveFromClient and dataToSendToClient to equals and toString
+    public int hashCode(){
+        return 696969;
+    }
+    public boolean equals(Object other){
+        ClackServer otherClackServer = (ClackServer)other;
+        return (this.port == otherClackServer.port && this.closeConnection == otherClackServer.closeConnection);
+    }
+    public String toString(){
+        return ("Port: " + this.port);
+    }
 }
