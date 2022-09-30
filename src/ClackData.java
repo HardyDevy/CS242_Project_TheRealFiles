560a0f4e62686e78234f04a0a1dac7a74d749938
HardyDevy <dghardy2020@gmail.com>
2022-09-30 14:18:13 -0400

    Implements all methods (except hashcode n some other stuff)

--- src/ClackData.java
+++ src/ClackData.java
@@ -1,13 +1,33 @@
-public class ClackData {
-    String username;
-    int type;
-    //date
+import java.util.Date;
 
-    //ClackData(userName, type )
-    //ClackData( type )
-    //ClackData()
-    //getType()
-    //getUserName()
-    //getDate()
-    //getData()
+public abstract class ClackData {
+    //ClackData
+    public String userName;
+    public int type;
+    public Date date;
+
+    public ClackData(String userName, int type) {
+        this.userName = userName;
+        this.type = type;
+    };
+    public ClackData(int type){
+        this("Anon", type);
+    }
+
+    public ClackData(){
+        this("Anon", 5);
+    }
+
+
+    public int getType(){
+        return this.type;
+    }
+    public String getuserName(){
+        return this.userName;
+    }
+    public Date getDate(){
+        return this.date;
+    }
+
+    //public abstract Data getData(){}
 }
--- src/MessageClackData.java
+++ src/MessageClackData.java
@@ -1,10 +1,27 @@
-public class MessageClackData {
-    String message;
+public abstract class MessageClackData extends ClackData{
+    //MessageClackData
+    private String message;
 
-    //MessageClackData( userName, message, type )
-    //MessageClackData()
-    //getData()
-    //hashCode()
-    //equals()
-    //toString()
+    public MessageClackData(String userName, String message, int type){
+        super(userName, type);
+        this.message = message;
+    }
+    public MessageClackData(){
+        this("Anon","Error",5);
+    }
+    //public abstract Data getData();
+
+    public int hashCode(){
+        return 0x800;
+    }
+    public boolean equals(Object other){
+        MessageClackData otherMessage = (MessageClackData) other;
+        return this.userName == otherMessage.userName &&
+                this.message == otherMessage.message &&
+                this.type == otherMessage.type;
+    }
+
+    //toString(){}
+
+
 }
